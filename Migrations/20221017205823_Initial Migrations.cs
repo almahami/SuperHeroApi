587f1b4a223c16cd8cb46ef3b6ad4a1c3c0dcf01
AL Mahamid, Mohammad <MohammadAl.Mahamid@vector.com>
2022-10-17 23:07:10 +0200

    add Migration

---
+++ Migrations/20221017205823_Initial Migrations.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperHeroApi.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Superheroes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Height = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Superheroes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Instructor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SuperheroId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Superheroes_SuperheroId",
+                        column: x => x.SuperheroId,
+                        principalTable: "Superheroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Superpowers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SuperPower = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SuperheroId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Superpowers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Superpowers_Superheroes_SuperheroId",
+                        column: x => x.SuperheroId,
+                        principalTable: "Superheroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Superheroes",
+                columns: new[] { "Id", "Description", "Height", "Name" },
+                values: new object[] { new Guid("609216a3-134f-44e5-85ae-5a48cad23a43"), "Black Widow, real name Natasha Romanoff, is a trained female secret agent and superhero that appears in Marvel Comics. Associated with the superhero teams S.H.I.E.L.D. and the Avengers, Black Widow makes up for her lack of superpowers with world class training as an athlete, acrobat, and expert martial artist and weapon specialist.", 1.7, "Black Widow" });
+
+            migrationBuilder.InsertData(
+                table: "Superheroes",
+                columns: new[] { "Id", "Description", "Height", "Name" },
+                values: new object[] { new Guid("68fde265-dcb9-4568-ab3c-64380e4a91b2"), "Batman was originally introduced as a ruthless vigilante who frequently killed or maimed criminals, but evolved into a character with a stringent moral code and strong sense of justice. Unlike most superheroes, Batman does not possess any superpowers, instead relying on his intellect, fighting skills, and wealth.", 1.9299999999999999, "Batman" });
+
+            migrationBuilder.InsertData(
+                table: "Superheroes",
+                columns: new[] { "Id", "Description", "Height", "Name" },
+                values: new object[] { new Guid("7f7687a1-90ed-4f41-96e3-52ee2bf5ec0f"), "Luke Skywalker was a Tatooine farmboy who rose from humble beginnings to become one of the greatest Jedi the galaxy has ever known. Along with his friends Princess Leia and Han Solo, Luke battled the evil Empire, discovered the truth of his parentage, and ended the tyranny of the Sith.", 1.7, "Luke Skywalker" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Instructor", "ReleaseDate", "SuperheroId", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("347f946f-5a96-4ffd-88e1-f64df4d24378"), "The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner and written by Leigh Brackett and Lawrence Kasdan, based on a story by George Lucas.", "Irvin Kershner", new DateTime(1980, 5, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("7f7687a1-90ed-4f41-96e3-52ee2bf5ec0f"), "Star Wars: Episode V – The Empire Strikes Back" },
+                    { new Guid("368c4f51-2524-439a-a775-c958289259df"), "The Dark Knight Rises is a 2012 superhero film directed by Christopher Nolan, who co-wrote the screenplay with his brother Jonathan Nolan, and the story with David S. Goyer.", "Christopher Nolan", new DateTime(2012, 7, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("68fde265-dcb9-4568-ab3c-64380e4a91b2"), "The Dark Knight Rises" },
+                    { new Guid("7f0e2729-7767-4fd1-b7d6-c251e7c22261"), "Star Wars (retroactively titled Star Wars: Episode IV – A New Hope) is a 1977 American epic space opera film written and directed by George Lucas, produced by Lucasfilm and distributed by 20th Century Fox.", "George Lucas", new DateTime(1977, 5, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("7f7687a1-90ed-4f41-96e3-52ee2bf5ec0f"), "Star Wars: Episode IV – A New Hope" },
+                    { new Guid("8b6dee1d-b928-4c7e-92c1-12d4b58541c3"), "The Dark Knight is a 2008 superhero film directed, produced, and co-written by Christopher Nolan. Based on the DC Comics character Batman, the film is the second installment of Nolan's The Dark Knight Trilogy and a sequel to 2005's Batman Begins, starring Christian Bale and supported by Michael Caine, Heath Ledger, Gary Oldman, Aaron Eckhart, Maggie Gyllenhaal, and Morgan Freeman.", "Christopher Nolan", new DateTime(2008, 7, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("68fde265-dcb9-4568-ab3c-64380e4a91b2"), "The Dark Knight" },
+                    { new Guid("deb8e69f-b2f5-4ee7-9384-fa4f5916d030"), "Black Widow is a 2021 American superhero film based on Marvel Comics featuring the character of the same name. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, it is the 24th film in the Marvel Cinematic Universe (MCU).", "Cate Shortland", new DateTime(2021, 6, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("609216a3-134f-44e5-85ae-5a48cad23a43"), "Black Widow" },
+                    { new Guid("e32ea97b-b647-4660-969e-d0d851e13e4f"), "Batman Begins is a 2005 superhero film directed by Christopher Nolan and written by Nolan and David S. Goyer. Based on the DC Comics character Batman, it stars Christian Bale as Bruce Wayne / Batman, with Michael Caine, Liam Neeson, Katie Holmes, Gary Oldman,", "Christopher Nolan", new DateTime(2005, 6, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("68fde265-dcb9-4568-ab3c-64380e4a91b2"), "Batman Begins" },
+                    { new Guid("f9f5afad-0bb9-42aa-a627-bfcb5eee4048"), "Return of the Jedi (also known as Star Wars: Episode VI – Return of the Jedi) is a 1983 American epic space opera film directed by Richard Marquand. The screenplay is by Lawrence Kasdan and George Lucas from a story by Lucas, who was also the executive producer.", "Richard Marquand", new DateTime(1983, 5, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("7f7687a1-90ed-4f41-96e3-52ee2bf5ec0f"), "Star Wars: Episode VI – Return of the Jedi" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Superpowers",
+                columns: new[] { "Id", "Description", "SuperPower", "SuperheroId" },
+                values: new object[,]
+                {
+                    { new Guid("093ec8e4-cb84-45ff-b635-dc98223cc25a"), "Skywalker is able to deflect fire from a blaster back at the opponent firing. This enables Luke to turn someone else's weapon against them.", "Deflect blaster power.", new Guid("7f7687a1-90ed-4f41-96e3-52ee2bf5ec0f") },
+                    { new Guid("3f2acbc7-5e96-45b6-ba3c-a1142754d01e"), "Sublime fighting skills.", "Fighting", new Guid("68fde265-dcb9-4568-ab3c-64380e4a91b2") },
+                    { new Guid("45a8d617-24d1-4173-915a-867ed285e158"), "The knowledge of how to undermine others.", "Subterfuge", new Guid("609216a3-134f-44e5-85ae-5a48cad23a43") },
+                    { new Guid("542ca629-fd20-490d-a299-bbc9e490e320"), "He's always a step ahead.", "Intellect.", new Guid("68fde265-dcb9-4568-ab3c-64380e4a91b2") },
+                    { new Guid("9114918c-a096-46dc-be7f-bdf5d1d0b08d"), "She's good at spying at people.", "Espionage", new Guid("609216a3-134f-44e5-85ae-5a48cad23a43") },
+                    { new Guid("be51931a-80d0-4cec-9ea0-a7262e010cb0"), "He got a lot of money", "Wealth.", new Guid("68fde265-dcb9-4568-ab3c-64380e4a91b2") },
+                    { new Guid("f7c13557-aa94-4fe8-9f19-40e96a223f15"), "She knows how to infiltrate the enemy.", "Infiltration", new Guid("609216a3-134f-44e5-85ae-5a48cad23a43") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_SuperheroId",
+                table: "Movies",
+                column: "SuperheroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Superpowers_SuperheroId",
+                table: "Superpowers",
+                column: "SuperheroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Superpowers");
+
+            migrationBuilder.DropTable(
+                name: "Superheroes");
+        }
+    }
+}
